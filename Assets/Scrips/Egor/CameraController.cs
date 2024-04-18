4885ae1c72980d88714e8aade60d1a0f5a0a8a60
mivoky <66131852+mivoky@users.noreply.github.com>
2024-04-18 22:25:40 +0300

    Смена камеры на букву G
    При нажатии буквы G камера меняется с одной на вторую.
    
    Сцена: CameraTesting(Egor)
    
    Незначительные изменения:
    1. Перенёс мем из папки Scene в Texture

---
+++ Assets/Scrips/Egor/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera MainCamera;
+    public Camera SkyCamera;
+
+    private bool _skyCameraBool = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCamera.enabled = true;
+        SkyCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeCamera();
+    }
+
+    private void ChangeCamera()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            _skyCameraBool = !_skyCameraBool;
+            Debug.Log(_skyCameraBool);
+        }
+        MainCamera.enabled = !_skyCameraBool;
+        SkyCamera.enabled = _skyCameraBool;
+    }
+}
