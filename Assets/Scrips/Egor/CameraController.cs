29e4f1132ae787416926ebeab3759322171dd056
mivoky <66131852+mivoky@users.noreply.github.com>
2024-04-19 18:10:03 +0300

    Передвижение камеры в небе
    После перехода на "Небесную" камеру открывается возможность перемещать камеру на WASD.
    
    Сцена: CameraTesting(Egor)
    
    Требует доработки: при выходе из небесной камеры обнулять координаты небесной камеры относительно игрока.

--- Assets/Scrips/Egor/CameraController.cs
+++ Assets/Scrips/Egor/CameraController.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public Camera MainCamera;
     public Camera SkyCamera;
+    public Transform TransformSkyCamera;
+
+    public float SpeedSkyCamera;
 
     private bool _skyCameraBool = false;
+
+    private float _moveHorizontal;
+    private float _moveVertical;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +27,10 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         ChangeCamera();
+        if (SkyCamera.enabled == true)
+        {
+            MoveSkyCamera();
+        }
     }
 
     private void ChangeCamera()
@@ -27,9 +38,18 @@ public class CameraController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.G))
         {
             _skyCameraBool = !_skyCameraBool;
-            Debug.Log(_skyCameraBool);
         }
         MainCamera.enabled = !_skyCameraBool;
         SkyCamera.enabled = _skyCameraBool;
+        //Debug.Log(_skyCameraBool);
+    }
+
+    private void MoveSkyCamera()
+    {
+        _moveHorizontal = Input.GetAxis("Horizontal");
+        _moveVertical = Input.GetAxis("Vertical");
+        TransformSkyCamera.position = new Vector3(TransformSkyCamera.position.x + SpeedSkyCamera * _moveHorizontal * Time.deltaTime, TransformSkyCamera.position.y, TransformSkyCamera.position.z + SpeedSkyCamera * _moveVertical * Time.deltaTime);
+        //Debug.Log(Input.GetAxis("Horizontal"));
+        //Debug.Log(Input.GetAxis("Vertical"));
     }
 }
