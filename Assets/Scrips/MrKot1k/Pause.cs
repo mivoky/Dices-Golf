970f9db7ea2dc3b3a3258c98911fc37b51ed0ea5
MrKot1k <158545943+MrKot1k@users.noreply.github.com>
2024-05-03 09:45:00 +0300

    наработки с паузой

--- Assets/Scrips/MrKot1k/Pause.cs
+++ Assets/Scrips/MrKot1k/Pause.cs
@@ -7,12 +7,18 @@ public class Pause : MonoBehaviour
 {
     public GameObject panel;
 
-    public void pause()
+    void Update()
     {
-        Input.GetKeyDown(KeyCode.Escape);
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             panel.SetActive(true);
             Time.timeScale = 0;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            panel.SetActive(false);
+            Time.timeScale = 1f;
         }
     }
 }
