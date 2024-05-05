5a153f2e75f5cf324dc01e20debc6349b11fc804
Maxim435r780 <159707362+Maxim435r780@users.noreply.github.com>
2024-05-05 21:24:21 +0300

    Добавление сцен с уровнями, переход между уровнями
    Небольшая доработка уровня долина

---
+++ Assets/Scrips/Maxim/LevelEnd.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnd : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        var PlayerScript = collision.gameObject.GetComponent<AddPulseObject>();
+        if (PlayerScript != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
