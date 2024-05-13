9e10a6b56fef19238440580cf932469d8705c3ab
mivoky <66131852+mivoky@users.noreply.github.com>
2024-05-13 17:56:45 +0300

    Начало работы над Д6

---
+++ Assets/Scrips/Egor/D6.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class D6 : MonoBehaviour
+{
+    public float Speed = 3f;
+    public float LiftUp;
+    private bool _start = false;
+    private int _effect;
+    private Vector3 _endPosition;
+    void Start()
+    {
+        _endPosition = new Vector3(transform.position.x, transform.position.y + LiftUp, transform.position.z);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _start = true;
+    }
+    void Update()
+    {
+        if (_start)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _endPosition, Speed * Time.deltaTime);
+        }
+        if (transform.position == _endPosition && _start)
+        {
+            _effect = Random.Range(1, 7);
+            Destroy(gameObject);
+        }
+    }
+}
