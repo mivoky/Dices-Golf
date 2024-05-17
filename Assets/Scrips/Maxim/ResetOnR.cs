ba9880c819d5c4b6f4503c70acad345c4b885b44
Maxim435r780 <159707362+Maxim435r780@users.noreply.github.com>
2024-05-17 19:34:04 +0300

    Телепорт в начало уровня при зажатии R

---
+++ Assets/Scrips/Maxim/ResetOnR.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetOnR : MonoBehaviour
+{
+    private float t = 0.0f;
+    [SerializeField] private GameObject ImgObject;
+    [SerializeField] private Transform PlayerTrans;
+    [SerializeField] private Rigidbody PlayerRigBod;
+    [SerializeField] private Transform ResetPoint;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            t = 0;
+            ImgObject.SetActive(true);
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            t += (1 * Time.deltaTime);
+            t = Mathf.Min(t, 2);
+            ImgObject.GetComponent<Image>().color = Color.Lerp(Color.white, Color.red, t / 2);
+            if (t >= 2)
+            {
+                t = 0;
+                StartCoroutine(ResetPos());
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            ImgObject.SetActive(false);
+        }
+    }
+    IEnumerator ResetPos()
+    {
+        PlayerTrans.position = ResetPoint.position;
+        PlayerRigBod.isKinematic = true;
+        yield return new WaitForSeconds(0.01f);
+        PlayerRigBod.isKinematic = false;
+    }
+}
