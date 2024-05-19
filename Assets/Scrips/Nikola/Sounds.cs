7c4be6f7f461365cdbe57e8b3336dba2a7edb21d
TheLiteMan <159713625+TheLiteMan@users.noreply.github.com>
2024-05-19 18:40:58 +0300

    Звук броска и подбора кубика

---
+++ Assets/Scrips/Nikola/Sounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PulseSound();
+    }
+
+    private void PulseSound()
+    {
+        var audioSource = GetComponent<AudioSource>();
+        if (Input.GetMouseButtonUp(0))
+        {
+            audioSource.Play(0);
+        }
+    }
+}
---
+++ Assets/Scrips/Nikola/SoundsD6.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsD6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var audioSource = GetComponent<AudioSource>();
+        audioSource.Play(0);
+    }
+}
