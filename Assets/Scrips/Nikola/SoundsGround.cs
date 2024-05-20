7c1784c01387fde12bc1894d807b15b90de67918
TheLiteMan <159713625+TheLiteMan@users.noreply.github.com>
2024-05-20 15:15:22 +0300

    Звук приземления

---
+++ Assets/Scrips/Nikola/SoundsGround.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsGround : MonoBehaviour
+{
+    public bool IsGrounded()
+    {
+        RaycastHit hit;
+        float rayLength = 1.9f; // Adjust based on your character's size
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, rayLength))
+        {
+            return true;
+        }
+        return false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GroundSound();
+    }
+
+    private void GroundSound()
+    {
+        var audioSource = GetComponent<AudioSource>();
+        if (IsGrounded() == false)
+        {
+            audioSource.Play(0);
+        }
+    }
+}
