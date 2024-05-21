eac9284ccec8d3dc693684c8aaab366d515a3676
MrKot1k <158545943+MrKot1k@users.noreply.github.com>
2024-05-21 22:18:33 +0300

    замучали работой

--- Assets/Scrips/MrKot1k/MoveToScene0.cs
+++ Assets/Scrips/MrKot1k/MoveToScene0.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour
+public class MoveToScene0 : MonoBehaviour
 {
     public void MoveToScene(int SceneID)
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
     
+
 }
