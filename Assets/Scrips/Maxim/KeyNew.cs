74fc79f27df32111ab236904e269c5b9878480d8
Maxim435r780 <159707362+Maxim435r780@users.noreply.github.com>
2024-05-21 23:45:17 +0300

    УровеньДанж + ключ + звук ключа

---
+++ Assets/Scrips/Maxim/KeyNew.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyNew : MonoBehaviour
+{
+    public float RotateSpeed = 100.0f;
+    [SerializeField] private GameObject _DisableOnCollection;
+    [SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField] private AudioSource _AudSource;
+
+    //поворот ключа
+    void Update()
+    {
+        transform.Rotate(0, 0, RotateSpeed * Time.deltaTime);
+    }
+
+    //сбор ключа
+    private void OnTriggerEnter(Collider other)
+    {
+        {
+            var PlayerScript = other.gameObject.GetComponent<AddPulseObject>();
+            if (PlayerScript != null)
+            {
+                _DisableOnCollection.SetActive(false);
+                _AudSource.Play();
+                Destroy(_particleSystem);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
