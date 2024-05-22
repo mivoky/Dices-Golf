1c485fe6b62337523b8f7fb6481d9ff17003af12
Maxim435r780 <159707362+Maxim435r780@users.noreply.github.com>
2024-05-22 04:01:43 +0300

    Невидимые стены + Д6 со скриптом "ключа"

--- Assets/Scrips/Maxim/KeyBehaviour.cs
+++ Assets/Scrips/Maxim/KeyBehaviour.cs
@@ -4,26 +4,15 @@ using UnityEngine;
 
 public class KeyBehaviour : MonoBehaviour
 {
-    public float RotateSpeed = 100.0f;
-    [SerializeField] private LevelEnd _levelEnd;
-    [SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField] private LevelEnd _SphereOfEndLevel;
 
-    //поворот ключа
-    void Update()
-    {
-        transform.Rotate(0, 0, RotateSpeed * Time.deltaTime);
-    }
-
-    //сбор ключа
     private void OnTriggerEnter(Collider other)
     {
         {
             var PlayerScript = other.gameObject.GetComponent<AddPulseObject>();
             if (PlayerScript != null)
             {
-                _levelEnd.DecreaseKeysCount();
-                Destroy(_particleSystem);
-                Destroy(gameObject);
+                _SphereOfEndLevel.DecreaseKeysCount();
             }
         }
     }
